8c64c8f1e85134bb0f4b65a352bbe8afc21aceb1
Rajini Mohanram <rajini.mohanram@cognizant.com>
2022-03-12 19:23:31 +0530

    Eureka Server without gateway and loadbalancer

---
+++ src/test/java/com/srt/project/MicroserviceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srt.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
